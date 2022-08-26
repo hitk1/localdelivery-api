836639c9c69c700329ead028515ad30932a0b0f2
hitk1 <luispaulo.degini@gmail.com>
2022-08-25 22:20:25 -0300

    feat: endpoint para criacao dos dados básicos

--- lib/deliveryapi/modules/customers/services/_onboarding/create.ex
+++ lib/deliveryapi/modules/customers/services/_onboarding/create.ex
@@ -1,13 +1,50 @@
 defmodule Customers.Services.Onboarding.CreateBaseData do
+  import Ecto.Query, only: [from: 2]
+
   alias Customers.Repo.Customer
   alias Deliveryapi.Repo
 
-  def call(params) do
-    with {:ok, %Ecto.Changeset{valid?: true} = changeset} <-
-           Customer.changeset(%{action: "create_base_data", params: params}) do
-      Repo.insert(changeset)
-    else
-      error -> deal_error(error)
+  def call(%{"email" => user_email} = params) do
+    case Customer.changeset(%{action: "create_base_data", params: params}) do
+      {:ok, %Ecto.Changeset{valid?: true} = changeset} ->
+        with %Customer{} = found_user <-
+               Repo.get_by(Customer, email: user_email, is_active: false) do
+          update_customer(params, Map.get(found_user, :id))
+        else
+          nil ->
+            case Repo.insert(changeset) do
+              {:ok, created_customer} -> {:ok, created_customer, :created}
+            end
+        end
+
+      {:ok, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  defp update_customer(
+         %{
+           "name" => name,
+           "email" => email,
+           "phone_number" => phone_number
+         } = params,
+         customer_id
+       ) do
+    update_query =
+      from(c in Customer,
+        where: c.id == ^customer_id,
+        update: [
+          set: [
+            name: ^name,
+            email: ^email,
+            phone_number: ^phone_number
+          ]
+        ]
+      )
+
+    case Repo.update_all(update_query, []) do
+      {1, nil} -> {:ok, %{id: customer_id}, :ok}
+      error -> {:error, "Error on update existing user"}
     end
   end
 
--- lib/deliveryapi_web/controllers/customers/onboarding_controller.ex
+++ lib/deliveryapi_web/controllers/customers/onboarding_controller.ex
@@ -7,9 +7,9 @@ defmodule DeliveryapiWeb.CustomerOnboardingController do
   action_fallback FallbackController
 
   def create_base_data(conn, params) do
-    with {:ok, %{id: id}} <- Deliveryapi.create_customer_base_data(params) do
+    with {:ok, %{id: id}, status_code} <- Deliveryapi.create_customer_base_data(params) do
       conn
-      |> put_status(:created)
+      |> put_status(status_code)
       |> render("customer_base_data_created.json", user_id: id)
     end
   end
